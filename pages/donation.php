a0ad641038e7ea3d3bbb1b02bb6f4a2f8818c4eb
DedunuDivyanjalee <divyanjaleededunu@gmail.com>
2024-10-31 17:31:01 +0530

    donation page created

---
+++ pages/donation.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="container-fluid px-2 my-4 g-3 border border-light">
+        <div class="row">
+            <!-- Left Column -->
+            <div class="col-md-6">
+
+<div class="container border border-dark p-4 my-5">
+    <h2 class="mb-3">Donate</h2>
+    <p>Pawsitive_Home was founded in 2015 to improve the well-being of street pets in Sri Lanka</p>
+</div> 
+<div class="container border border-dark p-4 my-5">   
+    <h2 class="mt-4">Donation Information</h2>
+    <p>I would like to make a donation in the amount of (LKR):</p>
+
+    
+    <div class="form-check mb-2">
+        <input type="radio" class="form-check-input" name="amount" id="a1000">
+        <label class="form-check-label" for="a1000">Rs 1000</label><br>
+    
+        <input type="radio" class="form-check-input" name="amount" id="a2500">
+        <label class="form-check-label" for="a2500">Rs 2500</label><br>
+    
+        <input type="radio" class="form-check-input" name="amount" id="a5000">
+        <label class="form-check-label" for="a5000">Rs 5000</label><br>
+    
+        <input type="radio" class="form-check-input" name="amount" id="a10000">
+        <label class="form-check-label" for="a10000">Rs 10000</label><br>
+
+        <input type="radio" class="form-check-input" name="amount" id="a20000">
+        <label class="form-check-label" for="a20000">Rs 20000</label><br>
+</div>
+    
+    <div class="form-check">
+        <input type="radio" class="form-check-input" name="amount" id="aOther">
+        <label class="form-check-label" for="aOther">
+            Other Rs: <input type="text" class="form-control d-inline w-auto ms-2" name="other" placeholder="Enter amount">
+        </label>
+    </div>
+</div>
+
+<div class="container border border-dark p-4 my-5">
+    <h2 class="mb-3">I would like my donations to be used for</h2>
+    
+    <div class="form-check">
+        <input type="radio" class="form-check-input" name="pet" id="pawsitive">
+        <label class="form-check-label" for="pawsitive">Pawsitive_Home</label>
+    </div>
+</div>
+
+<div class="container border border-dark p-4 my-5">
+    <h2>Donor Information</h2>
+
+    <form>
+        <div class="mb-2">
+            <label class="form-label" for="firstName">First Name:</label>
+            <input type="text" class="form-control" id="firstName" name="firstName">
+        
+
+        
+            <label class="form-label" for="lastName">Last Name:</label>
+            <input type="text" class="form-control" id="lastName" name="lastName">
+        
+
+        <p style="color:darkred;"><i>*Please give the address that is associated with your card</i></p>
+
+       
+            <label class="form-label" for="country">Country:</label>
+            <input type="text" class="form-control" id="country" name="country">
+        
+            <label class="form-label" for="address">Street Address:</label>
+            <input type="text" class="form-control" id="address" name="address">
+     
+            <label class="form-label" for="city">City:</label>
+            <input type="text" class="form-control" id="city" name="city">
+        
+            <label class="form-label" for="state">State/Province:</label>
+            <input type="text" class="form-control" id="state" name="state">
+       
+            <label class="form-label" for="zip">Zip/Postal Code:</label>
+            <input type="text" class="form-control" id="zip" name="zip">
+        
+            <label class="form-label" for="contactNo">Contact No:</label>
+            <input type="text" class="form-control" id="contactNo" name="contactNo">
+        
+            <label class="form-label" for="email">Email:</label>
+            <input type="email" class="form-control" id="email" name="email">
+    
+            <label class="form-label" for="remarks">Remarks:</label>
+            <textarea class="form-control" cols="20" rows="3">
+</textarea><br>
+
+<input type="checkbox" name="agree">
+<label class="form-label" for="agree">Donate as a gift</label>
+
+        </div>
+    </form>
+</div>
+
+<div class="container border border-dark p-4 my-5">
+    <h2 class="mb-3">Payment Option</h2>
+    
+    <form>
+        <div class="form-check mb-2">
+            <input type="radio" class="form-check-input" name="payment" id="visaMaster">
+            <label class="form-check-label" for="visaMaster">Visa / Master</label>
+        </div>
+
+        <div class="form-check mb-2 ">
+            <input type="radio" class="form-check-input" name="payment" id="paypal">
+            <label class="form-check-label" for="paypal">Paypal</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary mt-3">DONATE</button>
+    </form>
+</div>
+</div>
+
+
+<div class="col-md-6">
+    <div>
+        <br><br>
+    <h2>Security Guarantee</h2>
+    <hr>
+    <p>For youe protection and peace of mind, all transactions are encrypted and processed on a secure server, so you can submit your credit card details here with confidence.</p>
+
+    <img src="../images/donation/payment-logo.png" alt="payment_details" style="width:400px; height:70px;display: block;
+    margin: 0 auto;">
+</div>
+<br>
+    <div>
+        <h2>Bank Details</h2>
+        <hr>
+        <br>
+        <p>A/C name: Pawsitive_Home<br>
+           Acc No: 011 890890<br>
+           Bank name: People's Bank<br>
+           Branch: Colombo<br>
+           Bank Code: 7278<br>
+           Branch Code: 175<br>
+           Switch Code: BSAMLKLX</p>
+    </div>
+</div>  
+</div></div>
+</body>
+</html> 
